95c12045abf8741e78ca3b5d4dbd98f370d86606
Manoj Aher <manojaher88@gmail.com>
2021-03-03 10:49:26 +0530

    Initial Commit

---
+++ FirstFramework/FirstFramework.h
@@ -0,0 +1,18 @@
+//
+//  FirstFramework.h
+//  FirstFramework
+//
+//  Created by Manoj Aher on 03/03/21.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for FirstFramework.
+FOUNDATION_EXPORT double FirstFrameworkVersionNumber;
+
+//! Project version string for FirstFramework.
+FOUNDATION_EXPORT const unsigned char FirstFrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <FirstFramework/PublicHeader.h>
+
+
